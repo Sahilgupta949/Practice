002d494fe14dc7eed0f128ce65b49c6623062bec
Sahil Gupta <sahil.g@dntinfotech.com>
2026-08-02 20:15:54 +0530

    Issue solved

--- My_Project.py
+++ My_Project.py
@@ -1,10 +1,12 @@
 name = "sahil"
 Age = 23
 print(name)
+
 print(Age)
 
 A = f"{name} is {Age} years old."
 print(A)
 
-message = f"{name} is {age} years old."
+message = f"{name} is {Age} years old."
 print(message)
+
